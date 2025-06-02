358878dedc5904c62a724080937c9ad534c69c38
ghd5100 <ghd5100@gmail.com>
2025-06-02 11:11:14 +0900

    modify project

--- src/test/java/actions/AppWithActionsApplicationTests.java
+++ src/test/java/actions/AppWithActionsApplicationTests.java
@@ -1,10 +1,10 @@
-package file;
+package actions;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Step16S3FileTApplicationTests {
+class AppWithActionsApplicationTests {
 
 	@Test
 	void contextLoads() {
